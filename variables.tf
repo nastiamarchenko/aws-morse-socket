54e0b3566b41aede9c3caa1d2bc2eef72aab8fd7
nastiamarchenko <48097866+nastiamarchenko@users.noreply.github.com>
2019-04-12 10:47:34 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,2 +1,14 @@
 variable "access_key" {}
 variable "secret_key" {}
+
+variable "aws_region1" {
+  default     = "us-east-1"
+}
+
+variable "aws_region2" {
+  default     = "eu-west-1"
+}
+
+variable "aws_region3" {
+  default     = "ap-southeast-1"
+}
