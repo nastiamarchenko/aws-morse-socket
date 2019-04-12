bc1e338cda6f3d4f132f3ac1d910041763e0059c
nastiamarchenko <48097866+nastiamarchenko@users.noreply.github.com>
2019-04-12 10:54:34 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -12,3 +12,48 @@ variable "aws_region2" {
 variable "aws_region3" {
   default     = "ap-southeast-1"
 }
+
+
+variable "az_count" {
+  description = "Number of AZs to cover in a given region"
+  default     = "2"
+}
+
+variable "app_image" {
+  description = "Docker image to run in the ECS cluster"
+  default     = "app/morse-code:latest"
+}
+
+variable "app_port" {
+  description = "Port exposed by the docker image to redirect traffic to"
+  default     = 5000
+}
+
+variable "app_count" {
+  description = "Number of docker containers to run"
+  default     = 3
+}
+
+variable "ecs_autoscale_role" {
+  description = "Role arn for the ecsAutocaleRole"
+  default     = "YOUR_ECS_AUTOSCALE_ROLE_ARN"
+}
+
+variable "ecs_task_execution_role" {
+  description = "Role arn for the ecsTaskExecutionRole"
+  default     = "YOUR_ECS_TASK_EXECUTION_ROLE_ARN"
+}
+
+variable "health_check_path" {
+  default = "/"
+}
+
+variable "fargate_cpu" {
+  description = "Fargate instance CPU units to provision (1 vCPU = 1024 CPU units)"
+  default     = "1024"
+}
+
+variable "fargate_memory" {
+  description = "Fargate instance memory to provision (in MiB)"
+  default     = "2048"
+}
