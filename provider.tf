ed7106c62dd1015716725302195cd55f504acc52
nastiamarchenko <48097866+nastiamarchenko@users.noreply.github.com>
2019-04-12 11:05:14 +0300

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -3,3 +3,24 @@ provider "aws" {
   secret_key = "${var.secret_key}"
   region     = "${var.aws_region1}"
 }
+
+resource "aws_iam_role" "iam_for_autoscale" {
+  name = "iam_for_autoscale"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "autoscaling.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
