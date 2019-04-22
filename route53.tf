c3bb1bf16fb9e4eed8e993f97f0aef59bf5281f8
nastiamarchenko <48097866+nastiamarchenko@users.noreply.github.com>
2019-04-22 17:20:54 +0300

    Create route53.tf

---
+++ route53.tf
@@ -0,0 +1,17 @@
+data "aws_route53_zone" "morse" {
+  name         = "amarchen.link."
+}
+
+resource "aws_route53_record" "morse" {
+  zone_id = "${data.aws_route53_zone.morse.zone_id}"
+  name    = "morse.amarchen.link"
+  type    = "A"
+  ttl     = "300"
+  records =  [ ["${module.cluster1.eip}"], ["${module.cluster2.eip}"], ["${module.cluster3.eip}"]]
+  latency_routing_policy {
+    region = "us-west-1"
+    #set_identifier  = "morse-us-west-1"
+  }
+  set_identifier  = "morse-us-west-1"
+
+}
